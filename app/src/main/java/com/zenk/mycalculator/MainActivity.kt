f3ee4311e823ed7e2bd070db694781c7b3778b12
Al Zenk <azenk@nicoletcollege.edu>
2023-06-09 15:29:17 -0500

    button functionality added.

--- app/src/main/java/com/zenk/mycalculator/MainActivity.kt
+++ app/src/main/java/com/zenk/mycalculator/MainActivity.kt
@@ -3,16 +3,69 @@ package com.zenk.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+    var lastNumeric : Boolean = false
+    var lastDecimal : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+
     }
 
     fun onDigit(view: View)
     {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDecimal = false
+    }
+
+    fun onClear(view: View)
+    {
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View)
+    {
+        //only allow one decimal point
+        if(lastNumeric && !lastDecimal)
+        {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDecimal = true
+        }
+    }
+
+    fun onOperator(view: View)
+    {
+        //let determines if tvInput.text exists.
+        tvInput?.text?.let{
+            if(lastNumeric && !isOperatorAdded(it.toString()))
+            {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDecimal = false
+            }
+        }
+
+    }
+    private fun isOperatorAdded(value : String) : Boolean
+    {
+        return if(value.startsWith("-"))
+        {
+            false
+        }
+        else
+        {
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+
+        }
     }
 }
